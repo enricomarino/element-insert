c7516f6274364bf5e007462f684f16388b1aba0d
Enrico Marino <enrico.marino@email.com>
2013-06-12 12:42:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,80 +1,97 @@
 /**
- * prepend
- * Prepend `element`.
- *
- * @param {Element} el
- * @return {Element} this for chaining
- * @api public
- */
-
-exports.prepend = function (element) {
-  var el = this.el;
-  if (el.children.length) {
-    el.insertBefore(element, el.firstChild);
-  } else {
-    el.appendChild(element);
-  }
-  return this;
-};
-
-/**
- * prepend
- * Prepend `element`.
- *
- * @param {Element} element
- * @return {Element} this for chaining
- * @api public
- */
-
-exports.append = function (element) {
-  var el = this.el;
-  el.appendChild(element);
-  return this;
-};
-
-/**
- * into
- * Append this `el` to `element`
- *
- * @param {Element} element element
- * @return {Element} this for chaining 
- * @api public
- */
-
-Element.prototype.into = function (element) {
-  element.append(this.el);
-  return this;
-};
-
-/**
- * before
- * Insert this `el` before `element`
+ * element-insert
+ * insert plugin for element
  * 
- * @param {Element} element element
- * @return {Element} this for chaining
- * @api public
+ * @copyright 2013 Enrico Marino
+ * @license MIT
  */
-
-exports.before = function (element) {
-  element.parentNode.insertBefore(this.el, element);
-  return this;
-};
 
 /**
- * after
- * Insert this `el` after `element`
- * 
- * @param {Element} element element
- * @return {Element} this for chaining
- * @api public
+ * Expose component
  */
 
-exports.after = function (element) {
-  var sibling = element.nextSibling;
-  if (sibling) {
-    element.parentNode.insertBefore(this.el, sibling);
-  } else {
-    element.parentNode.appendChild(this);
-  }
-  return this;
-};
+module.exports = function (element) {
+
+  /**
+   * prepend
+   * Prepend `element`.
+   *
+   * @param {Element} el
+   * @return {Element} this for chaining
+   * @api public
+   */
+  
+  element.prototype.prepend = function (other) {
+    var el = this.el;
+    if (el.children.length) {
+      el.insertBefore(other, el.firstChild);
+    } else {
+      el.appendChild(other);
+    }
+    return this;
+  };
+  
+  /**
+   * prepend
+   * Prepend `element`.
+   *
+   * @param {Element} element
+   * @return {Element} this for chaining
+   * @api public
+   */
+  
+  element.prototype.append = function (other) {
+    var el = this.el;
+    el.appendChild(other);
+    return this;
+  };
+  
+  /**
+   * into
+   * Append this `el` to `element`
+   *
+   * @param {Element} element element
+   * @return {Element} this for chaining 
+   * @api public
+   */
+  
+  element.prototype.into = function (other) {
+    other.append(this.el);
+    return this;
+  };
+  
+  /**
+   * before
+   * Insert this `el` before `element`
+   * 
+   * @param {Element} element element
+   * @return {Element} this for chaining
+   * @api public
+   */
+  
+  element.prototype.before = function (other) {
+    other.parentNode.insertBefore(this.el, other);
+    return this;
+  };
+  
+  /**
+   * after
+   * Insert this `el` after `element`
+   * 
+   * @param {Element} element element
+   * @return {Element} this for chaining
+   * @api public
+   */
+  
+  element.prototype.after = function (other) {
+    var sibling = other.nextSibling;
+    if (sibling) {
+      other.parentNode.insertBefore(this.el, sibling);
+    } else {
+      other.parentNode.appendChild(this.el);
+    }
+    return this;
+  };
+
+  return element;
+}
